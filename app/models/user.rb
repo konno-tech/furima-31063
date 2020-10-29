71006978ae25ec73a6538dd9c37f4f509b6714c2
konno-tech <kontc723@gmail.com>
2020-10-29 17:16:38 +0900

    単体テストの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,14 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: '英字と数字の両方を含めて設定してください' }
-  validates :password_confirmation, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: '英字と数字の両方を含めて設定してください' }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'は英字と数字の両方を含めて設定してください' }
+  validates :password_confirmation, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'は英字と数字の両方を含めて設定してください' }
 
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角（漢字・ひらがな・カタカナ）を使用してください' }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角（漢字・ひらがな・カタカナ）を使用してください' }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）を使用してください' }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）を使用してください' }
   
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角（カタカナ）を使用してください' }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角（カタカナ）を使用してください' }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'は全角（カタカナ）を使用してください' }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'は全角（カタカナ）を使用してください' }
 
   validates :birthday, presence: true
 
