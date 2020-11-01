6b6a1ef01233eaea60cb412cda9400e316c8d5b1
konno-tech <kontc723@gmail.com>
2020-11-01 18:21:05 +0900

    ActiveHash：カテゴリーの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,12 @@
 class Item < ApplicationRecord
   belongs_to       :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+
+  #空の投稿を保存できないようにする
+  validates :name, :description, :category, presence: true
+
+  #ジャンルの選択が「--」の時は保存できないようにする
+  validates :category_id, numericality: { other_than: 1 }
 end
