8e5be5d1c77ed7699eac8f24f968ace19239b044
konno-tech <kontc723@gmail.com>
2020-11-03 18:07:10 +0900

    商品出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,21 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @items = Item.new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :charge_of_distribution_cost_id,
+                                 :forwarding_origin_id, :before_forward_date_id, :price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,26 @@
 class Item < ApplicationRecord
   belongs_to       :user
   has_one_attached :image
+  
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :charge_of_distribution_cost
+  belongs_to_active_hash :forwarding_origin
+  belongs_to_active_hash :before_forward_date
 
   #空の投稿を保存できないようにする
-  validates :name, :description, :category_id, :condition_id, :charge_of_distribution_cost_id, 
-            :forwarding_origin_id, :before_forward_date_id, presence: true
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :category_id,                    numericality: { other_than: 1, message: 'Select' }
+    validates :condition_id,                   numericality: { other_than: 1, message: 'Select' }
+    validates :charge_of_distribution_cost_id, numericality: { other_than: 1, message: 'Select' }
+    validates :forwarding_origin_id,           numericality: { other_than: 1, message: 'Select' }
+    validates :before_forward_date_id,         numericality: { other_than: 1, message: 'Select' }
+    validates :price,                          inclusion: { in: 300..9999999 }, 
+                                               format: {with: /\A[0-9]+\z/, message: 'Half-width number'}
+  end
 
-  #ジャンルの選択が「--」の時は保存できないようにする
-  validates :category_id, numericality: { other_than: 1 }
 end
