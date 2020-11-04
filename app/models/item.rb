2cc6f0e4cabbb4deddaa9188e3cee28d3ada138a
konno-tech <kontc723@gmail.com>
2020-11-04 11:22:26 +0900

    itemモデルの単体テスト実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   belongs_to       :user
   has_one_attached :image
-  
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :condition
@@ -9,7 +9,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :forwarding_origin
   belongs_to_active_hash :before_forward_date
 
-  #空の投稿を保存できないようにする
+  # 空の投稿を保存できないようにする
   with_options presence: true do
     validates :image
     validates :name
@@ -19,8 +19,7 @@ class Item < ApplicationRecord
     validates :charge_of_distribution_cost_id, numericality: { other_than: 1, message: 'Select' }
     validates :forwarding_origin_id,           numericality: { other_than: 1, message: 'Select' }
     validates :before_forward_date_id,         numericality: { other_than: 1, message: 'Select' }
-    validates :price,                          inclusion: { in: 300..9999999 }, 
-                                               format: {with: /\A[0-9]+\z/, message: 'Half-width number'}
+    validates :price,                          numericality: { only_integer: true, message: 'Half-width number' },
+                                               inclusion: { in: 300..9_999_999 }
   end
-
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,20 @@
 FactoryBot.define do
   factory :item do
-    
+    name                           { Faker::String.random(length: 1..40) }
+    description                    { Faker::Lorem.sentence }
+    category_id                    { Faker::Number.between(from: 2, to: 11) }
+    condition_id                   { Faker::Number.between(from: 2, to: 7) }
+    charge_of_distribution_cost_id { Faker::Number.between(from: 2, to: 3) }
+    forwarding_origin_id           { Faker::Number.between(from: 2, to: 48) }
+    before_forward_date_id         { Faker::Number.between(from: 2, to: 4) }
+    price                          { Faker::Number.between(from: 300, to: 9_999_999) }
+
+    # itemから見てuserはbelongs_toの関係のため、userモデルとのアソシエーション定義
+    association :user
+
+    # 画像に関する設定
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.JPG'), filename: 'test_image.JPG')
+    end
   end
 end
