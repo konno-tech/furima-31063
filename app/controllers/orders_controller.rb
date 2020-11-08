2df6d807a8d7b338f0089280dfea6677b1fb2fdc
konno-tech <kontc723@gmail.com>
2020-11-08 18:05:28 +0900

    決済機能：環境変数の設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -31,12 +31,12 @@ class OrdersController < ApplicationController
   end
 
   def pay_item
-    Payjp.api_key = "sk_test_0f62717bee9c27d568d95964"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 秘密鍵
     Payjp::Charge.create(
       amount: @item.price,  # 商品の値段
       card: form_order_params[:token],    # カードトークン
-      currency: 'jpy'                 # 通貨の種類（日本円）
+      currency: 'jpy'                 # 日本円
     )
   end
-  
+
 end
